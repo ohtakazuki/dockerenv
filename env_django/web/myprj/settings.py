671effe1600984e19d852d90f2bc0d7d1b3b3294
ohtakazuki <ohtakazuki@gmail.com>
2021-04-15 16:56:24 +0900

    django/settings.pyからlogの記述を削除

--- env_django/web/myprj/settings.py
+++ env_django/web/myprj/settings.py
@@ -129,21 +129,3 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': './log/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
